668f3a7f7585e9014e86749ebeaf6ec6e9bd0c45
Andrew Krause <ajkrause@gmail.com>
2024-09-21 11:28:08 -0400

    Convert tabs -> spaces

--- assets/scripts/core/gameState.lua
+++ assets/scripts/core/gameState.lua
@@ -2,25 +2,25 @@ GameState = {}
 GameState.__index = GameState
 
 function setGameState(newState)
-	if (newState ~= nil) then
-		newState:start()
-		currentState = newState
-	else
-		error("INVALID STATE!")
-	end
+    if (newState ~= nil) then
+        newState:start()
+        currentState = newState
+    else
+        error("INVALID STATE!")
+    end
 end
 
 function GameState:new()
-	local this = {
-		start = function() end,
-		cancel = function() end,
-		click = function(x,y) end,
-		backPressed = function() end,
-		update = function() end,
-		render = function() end
-	}
-	setmetatable(this,self)
-	return this
+    local this = {
+        start = function() end,
+        cancel = function() end,
+        click = function(x,y) end,
+        backPressed = function() end,
+        update = function() end,
+        render = function() end
+    }
+    setmetatable(this,self)
+    return this
 end
 
 currentState = GameState:new()
--- assets/scripts/main.lua
+++ assets/scripts/main.lua
@@ -1,9 +1,9 @@
 --[[
 
-	Mustard Engine sample project
-	
-	Andrew Krause
-	2022.07.09
+    Mustard Engine sample project
+    
+    Andrew Krause
+    2022.07.09
 
 ]]
 
@@ -20,148 +20,148 @@ local timer = 0
 --  called once at engine startup just to set up parameters for the video subsystem.
 --  these could be read from a configuration file.
 function AB.loadConfig()
-	videoConfig = {
-		title = "SAMPLE PROJECT",
-		xRes = 800,
-		yRes = 480,
-		
-		xOffset = 0,
-		yOffset = 0,
-		xScale = 1,
-		yScale = 1,
-		
-		fullscreen = false,
-		vsync = true,
-	}
-	
-	if (FULLSCREEN) then
-		videoConfig.xRes, videoConfig.yRes = AB.graphics.getDesktopResolution()
-		videoConfig.fullscreen = true
-		
-		local xScale = videoConfig.xRes / CANVAS_WIDTH
-		local yScale = videoConfig.yRes / CANVAS_HEIGHT
-		videoConfig.scale = math.min(xScale, yScale)
-	else
-		videoConfig.xRes = CANVAS_WIDTH
-		videoConfig.yRes = CANVAS_HEIGHT
-		videoConfig.fullscreen = false
-		
-		videoConfig.scale = 1
-	end
+    videoConfig = {
+        title = "SAMPLE PROJECT",
+        xRes = 800,
+        yRes = 480,
+        
+        xOffset = 0,
+        yOffset = 0,
+        xScale = 1,
+        yScale = 1,
+        
+        fullscreen = false,
+        vsync = true,
+    }
+    
+    if (FULLSCREEN) then
+        videoConfig.xRes, videoConfig.yRes = AB.graphics.getDesktopResolution()
+        videoConfig.fullscreen = true
+        
+        local xScale = videoConfig.xRes / CANVAS_WIDTH
+        local yScale = videoConfig.yRes / CANVAS_HEIGHT
+        videoConfig.scale = math.min(xScale, yScale)
+    else
+        videoConfig.xRes = CANVAS_WIDTH
+        videoConfig.yRes = CANVAS_HEIGHT
+        videoConfig.fullscreen = false
+        
+        videoConfig.scale = 1
+    end
 
-	videoConfig.xOffset = (videoConfig.xRes - (CANVAS_WIDTH * videoConfig.scale)) / 2.0
-	videoConfig.yOffset = (videoConfig.yRes - (CANVAS_HEIGHT * videoConfig.scale)) / 2.0	
+    videoConfig.xOffset = (videoConfig.xRes - (CANVAS_WIDTH * videoConfig.scale)) / 2.0
+    videoConfig.yOffset = (videoConfig.yRes - (CANVAS_HEIGHT * videoConfig.scale)) / 2.0    
 end
 
---	called once after all engine subsystems have been initialized and we're ready to run.
---	this is where you'll want to load assets and set up state
+--    called once after all engine subsystems have been initialized and we're ready to run.
+--    this is where you'll want to load assets and set up state
 function AB.init()
-	-- seed the RNG and note the seed value for deterministic replays
-	AB.math.randomSeed()
-	AB.math.noiseSeed(AB.math.random(2 ^ 32 - 1))
+    -- seed the RNG and note the seed value for deterministic replays
+    AB.math.randomSeed()
+    AB.math.noiseSeed(AB.math.random(2 ^ 32 - 1))
 
-	local seed = AB.math.random(2 ^ 32 - 1)
-	AB.system.log("Random seed: " .. seed)
-	AB.math.randomSeed(seed)
+    local seed = AB.math.random(2 ^ 32 - 1)
+    AB.system.log("Random seed: " .. seed)
+    AB.math.randomSeed(seed)
 
-	-- load scripts
-	AB.system.loadScript("core/gameState.lua")
-	AB.system.loadScript("core/entityManager.lua")
-	AB.system.loadScript("core/vector2d.lua")
-	
-	AB.system.loadScript("gameStates/title.lua")
-	AB.system.loadScript("gameStates/game.lua")
-	AB.system.loadScript("gameStates/gameOver.lua")
-	
-	AB.system.loadScript("entities/player.lua")
-	AB.system.loadScript("entities/laser.lua")
-	AB.system.loadScript("entities/enemy.lua")
-	AB.system.loadScript("entities/splode.lua")
+    -- load scripts
+    AB.system.loadScript("core/gameState.lua")
+    AB.system.loadScript("core/entityManager.lua")
+    AB.system.loadScript("core/vector2d.lua")
+    
+    AB.system.loadScript("gameStates/title.lua")
+    AB.system.loadScript("gameStates/game.lua")
+    AB.system.loadScript("gameStates/gameOver.lua")
+    
+    AB.system.loadScript("entities/player.lua")
+    AB.system.loadScript("entities/laser.lua")
+    AB.system.loadScript("entities/enemy.lua")
+    AB.system.loadScript("entities/splode.lua")
 
-	-- load A/V assets
-	gfx = {
-		PLAYER = AB.graphics.loadSprite("gfx/player.tga"),
-		LASER = AB.graphics.loadSprite("gfx/laser.tga"),
-		ENEMY = AB.graphics.loadAtlas("gfx/enemy.tga", 40, 40, true)
-	}
+    -- load A/V assets
+    gfx = {
+        PLAYER = AB.graphics.loadSprite("gfx/player.tga"),
+        LASER = AB.graphics.loadSprite("gfx/laser.tga"),
+        ENEMY = AB.graphics.loadAtlas("gfx/enemy.tga", 40, 40, true)
+    }
 
-	sfx = {
-		START = AB.audio.loadSound("sfx/start.wav"),
-		LASER = AB.audio.loadSound("sfx/laser.wav"),
-		OUCH = AB.audio.loadSound("sfx/ouch.wav"),
-		SPLODE = AB.audio.loadSound("sfx/splode.wav"),
-		PLAYER_SPLODE = AB.audio.loadSound("sfx/playerSplode.wav")
-	}
-	
-	music = AB.audio.loadMusic("music/musicLoop.mp3", 4, 150)
-	AB.audio.playMusic(music)
+    sfx = {
+        START = AB.audio.loadSound("sfx/start.wav"),
+        LASER = AB.audio.loadSound("sfx/laser.wav"),
+        OUCH = AB.audio.loadSound("sfx/ouch.wav"),
+        SPLODE = AB.audio.loadSound("sfx/splode.wav"),
+        PLAYER_SPLODE = AB.audio.loadSound("sfx/playerSplode.wav")
+    }
+    
+    music = AB.audio.loadMusic("music/musicLoop.mp3", 4, 150)
+    AB.audio.playMusic(music)
 
-	font = AB.font.loadFont("default3")
-	
-	-- create layers and canvas
-	layers = {
-		DEFAULT = 0,
-		INTERFACE = AB.graphics.createLayer(1),
-		FLASHY = AB.graphics.createLayer(2),
-		SPRITES = AB.graphics.createLayer(3),
-		BACKGROUND = AB.graphics.createLayer(4)
-	}
-	canvas = AB.graphics.createCanvas(800, 480)
-	AB.input.showCursor(false)
-	
-	setGameState(title)
+    font = AB.font.loadFont("default3")
+    
+    -- create layers and canvas
+    layers = {
+        DEFAULT = 0,
+        INTERFACE = AB.graphics.createLayer(1),
+        FLASHY = AB.graphics.createLayer(2),
+        SPRITES = AB.graphics.createLayer(3),
+        BACKGROUND = AB.graphics.createLayer(4)
+    }
+    canvas = AB.graphics.createCanvas(800, 480)
+    AB.input.showCursor(false)
+    
+    setGameState(title)
 end
 
---	called at 60hz
+--    called at 60hz
 function AB.update()
-	timer = timer + 1
-	
-	currentState:update()
+    timer = timer + 1
+    
+    currentState:update()
 
-	AB.graphics.addColorTransform(layers.DEFAULT, AB.graphics.colorTransforms.HUE_SHIFT, 0.01)
+    AB.graphics.addColorTransform(layers.DEFAULT, AB.graphics.colorTransforms.HUE_SHIFT, 0.01)
 end
 
 --  it renders
 function AB.render()
-	AB.graphics.useCanvas(canvas)
-	AB.graphics.clear()
-	
-	--	draw background thinger
-	AB.graphics.setColor(0.15, 0.25, 0.25, 0.25, 1.0)
-	for y = 0, CANVAS_HEIGHT, 20 do
-		for x = 0, CANVAS_WIDTH, 20 do
-			-- these numbers are pretty arbitrary (futzed around until i found something that looked good)
-			local size = AB.math.noise(x / 1000, y / 1000, timer / 1000, 4, 8) * 30 + 20
-			AB.graphics.renderQuad(layers.BACKGROUND, size, size, x, y)
-		end
-	end
-	
-	currentState:render()
-	
-	AB.graphics.setColor()
-	AB.graphics.useCanvas(0)
+    AB.graphics.useCanvas(canvas)
+    AB.graphics.clear()
+    
+    --    draw background thinger
+    AB.graphics.setColor(0.15, 0.25, 0.25, 0.25, 1.0)
+    for y = 0, CANVAS_HEIGHT, 20 do
+        for x = 0, CANVAS_WIDTH, 20 do
+            -- these numbers are pretty arbitrary (futzed around until i found something that looked good)
+            local size = AB.math.noise(x / 1000, y / 1000, timer / 1000, 4, 8) * 30 + 20
+            AB.graphics.renderQuad(layers.BACKGROUND, size, size, x, y)
+        end
+    end
+    
+    currentState:render()
+    
+    AB.graphics.setColor()
+    AB.graphics.useCanvas(0)
 
-	AB.graphics.renderCanvas(layers.DEFAULT, canvas, videoConfig.xRes / 2, videoConfig.yRes / 2, -1, 0, videoConfig.scale)
+    AB.graphics.renderCanvas(layers.DEFAULT, canvas, videoConfig.xRes / 2, videoConfig.yRes / 2, -1, 0, videoConfig.scale)
 end
 
 -- you can optionally implement other callback functions
 function AB.onKeyPressed(key)
-	if (key == AB.input.scancodes.ESCAPE) then
-		if (currentState == title) then
-			AB.system.quit()
-		else
-			setGameState(title)
-		end
-	end
+    if (key == AB.input.scancodes.ESCAPE) then
+        if (currentState == title) then
+            AB.system.quit()
+        else
+            setGameState(title)
+        end
+    end
 end
 
 function AB.onGamepadPressed(index, button)
-	if (button == AB.input.gamepadButtons.BACK) then
-		if (currentState == title) then
-			AB.system.quit()
-		else
-			setGameState(title)
-		end
-	end
+    if (button == AB.input.gamepadButtons.BACK) then
+        if (currentState == title) then
+            AB.system.quit()
+        else
+            setGameState(title)
+        end
+    end
 end
 
